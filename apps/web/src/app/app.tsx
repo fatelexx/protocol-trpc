ddf5dcddca1f2f7d70dc554569f4d39c820c70f8
fatelexx <fatelexx@outlook.com>
2024-03-19 20:24:46 +0800

    chore: frontend setup

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,49 +1,14 @@
-import NxWelcome from './nx-welcome';
-
-import { Route, Routes, Link } from 'react-router-dom';
+import { trpc } from "../trpc";
 
 export function App() {
-  return (
-    <div>
-      <NxWelcome title="web" />
+  const onClick = async () => {
+    const user = await trpc.user.getUserById.query('0');
 
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
-    </div>
+    console.log(user);
+  };
+
+  return (
+    <button onClick={onClick}>Test Trpc</button>
   );
 }
 
